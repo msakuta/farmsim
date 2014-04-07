920fba9a02800f3b5d59137c61e6c64fa272de3b
msakuta <masahiro.sakuta@gmail.com>
2014-04-08 00:56:29 +0900

    Make grass growth be influenced by adjacent cells
    This way it's more natural to see grasses expand.

--- FarmGame.js
+++ FarmGame.js
@@ -35,11 +35,21 @@ FarmGame.prototype.onUpdateCell = function(cell,x,y){}
 
 FarmGame.prototype.update = function(){
 
-	var growth = 0.0001;
+	// The growth of the grass depends on adjacent cells' grass density.
+	function getGrowth(x,y){
+		var ret = 0.0001;
+		if(0 <= x - 1) ret += this.cells[x - 1][y].grass * 0.0001;
+		if(x + 1 < this.xs) ret += this.cells[x + 1][y].grass * 0.0001;
+		if(0 <= y - 1) ret += this.cells[x][y - 1].grass * 0.0001;
+		if(y + 1 < this.ys) ret += this.cells[x][y + 1].grass * 0.0001;
+		return ret;
+	}
 
 	for(var x = 0; x < this.cells.length; x++){
 		for(var y = 0; y < this.cells[x].length; y++){
 			var cell = this.cells[x][y];
+
+			var growth = getGrowth.call(this, x, y);
 
 			if(cell.grass < 1. - growth)
 				cell.grass += growth;
