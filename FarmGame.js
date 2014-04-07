ab42fdbe305e74e9a1b0d802804d706f8b58b829
msakuta <masahiro.sakuta@gmail.com>
2014-04-07 23:08:49 +0900

    Separate simulation core routine into FarmGame.js

---
+++ FarmGame.js
@@ -0,0 +1,44 @@
+
+function FarmGame(xs,ys){
+	this.xs = xs;
+	this.ys = ys;
+	this.rng = new Xor128();
+
+	this.cells = [];
+}
+
+FarmGame.prototype.init = function(){
+	for(var x = 0; x < this.xs; x++){
+		var row = [];
+		for(var y = 0; y < this.ys; y++){
+			var grass = this.rng.next();
+			var cell = {grass: grass};
+
+			this.onUpdateCell(cell,x,y);
+
+			row.push(cell);
+		}
+		this.cells.push(row);
+	}
+}
+
+FarmGame.prototype.onUpdateCell = function(cell,x,y){}
+
+FarmGame.prototype.update = function(){
+
+	var growth = 0.0001;
+
+	for(var x = 0; x < this.cells.length; x++){
+		for(var y = 0; y < this.cells[x].length; y++){
+			var cell = this.cells[x][y];
+
+			if(cell.grass < 1. - growth)
+				cell.grass += growth;
+			else
+				cell.grass = 1.;
+
+			game.onUpdateCell(cell,x,y);
+		}
+	}
+
+}
